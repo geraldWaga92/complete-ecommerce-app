56556ec9c84501a156e2de5f3cfc60e9a09811ff
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-04-28 15:30:39 -0700

    update the auths component and install jwt

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import morgan from "morgan";
 import cors from "cors";
+import authRoutes from "./routes/authRoute.js";
+
 
 //configure env
 dotenv.config();
@@ -20,6 +22,10 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
+//routes
+app.use("/api/v1/auth", authRoutes);
+// app.use("/api/v1/category", categoryRoutes);
+// app.use("/api/v1/product", productRoutes);
 
 
 
