6e4cbae3e4501ed1e0e3b6fcfa2bb8ca20e11e4c
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-05-01 10:05:06 -0700

    update header logo

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { GiShoppingBag } from "react-icons/gi"
+import Logo from "../../logofinal4.png"
 
 const Header = () => {
     return (
-        <div>Header</div>
-    )
-}
+        <>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarTogglerDemo01"
+                        aria-controls="navbarTogglerDemo01"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon" />
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                        <NavLink className="navbar-brand" to="/">
+                            <img
+                                src={Logo}
+                                class="img-fluid"
+                                width="150"
+                                height="50"
+                                alt="..." />
 
-export default Header
+
+                        </NavLink>
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <NavLink
+                                    className="nav-link isactive"
+                                    aria-current="page"
+                                    to="/"
+                                >
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/register">
+                                    Register
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/login">
+                                    Login
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/cart">
+                                    Cart (0)
+                                </NavLink>
+                            </li>
+
+                        </ul>
+
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+};
+
+export default Header;
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Layout from '../components/Layout/Layout'
 
 const HomePage = () => {
     return (
-        <div>HomePage</div>
+        <Layout>
+            <div>HomePage</div>
+        </Layout>
     )
 }
 
