3ae468258404cdc9098a30ce765e6eff05fe87d9
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-05-01 15:54:55 -0700

    create admin private routes and access with functionality

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,10 @@ import PrivateRoute from "./components/Routes/Private";
 import ForgotPasssword from "./pages/Auth/ForgotPassword";
 import AdminRoute from "./components/Routes/AdminRoute";
 import AdminDashboard from "./pages/Admin/AdminDashboard";
+import CreateCategory from "./pages/Admin/CreateCategory";
+import CreateProduct from "./pages/Admin/CreateProduct";
+import Products from "./pages/Admin/Products";
+import Users from "./pages/Admin/Users";
 
 function App() {
   return (
@@ -19,17 +23,17 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/dashboard" element={<PrivateRoute />}>
-          <Route path="" element={<Dashboard />} />
+          <Route path="user" element={<Dashboard />} />
           {/* <Route path="user/orders" element={<Orders />} /> */}
           {/* <Route path="user/profile" element={<Profile />} /> */}
         </Route>
         <Route path="/dashboard" element={<AdminRoute />}>
           <Route path="admin" element={<AdminDashboard />} />
-          {/* <Route path="admin/create-category" element={<CreateCategory />} /> */}
-          {/* <Route path="admin/create-product" element={<CreateProduct />} /> */}
+          <Route path="admin/create-category" element={<CreateCategory />} />
+          <Route path="admin/create-product" element={<CreateProduct />} />
           {/* <Route path="admin/product/:slug" element={<UpdateProduct />} /> */}
-          {/* <Route path="admin/products" element={<Products />} /> */}
-          {/* <Route path="admin/users" element={<Users />} /> */}
+          <Route path="admin/products" element={<Products />} />
+          <Route path="admin/users" element={<Users />} />
           {/* <Route path="admin/orders" element={<AdminOrders />} /> */}
         </Route>
         <Route path="/register" element={<Register />} />
