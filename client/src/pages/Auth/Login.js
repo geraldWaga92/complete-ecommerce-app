3566ae15f6130e74bedbe8732d26624a6f839e30
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-05-01 13:35:46 -0700

    update login component

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -30,6 +30,8 @@ const Login = () => {
                     user: res.data.user,
                     token: res.data.token
                 })
+                localStorage.setItem("auth", JSON.stringify(res.data))
+                navigate("/");
             } else {
                 toast.error(res.data.message);
             }
