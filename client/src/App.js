1ae1fb4c401783e142403e7a6b56645fc7c8504d
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-05-01 14:16:43 -0700

    update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,13 +8,18 @@ import PageNotFound from "./pages/PageNotFound";
 import Register from "./pages/Auth/Register";
 import Login from "./pages/Auth/Login";
 import Dashboard from "./pages/user/Dashboard";
+import PrivateRoute from "./components/Routes/Private";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<PrivateRoute />}>
+          {/* <Route path="user" element={<Dashboard />} /> */}
+          {/* <Route path="user/orders" element={<Orders />} /> */}
+          {/* <Route path="user/profile" element={<Profile />} /> */}
+        </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
