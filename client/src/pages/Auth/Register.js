088e190c92f3fbda03940854477d4d94afd1eae7
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-05-01 13:04:41 -0700

    update styles for register form

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -38,83 +38,86 @@ const Register = () => {
     };
 
     return (
-        <Layout title="Register - Gerald e-shop">
-            <div className="form-container" style={{ minHeight: "90vh" }}>
-                <form onSubmit={handleSubmit}>
-                    <h4 className="title">REGISTER FORM</h4>
-                    <div className="mb-3">
-                        <input
-                            type="text"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            className="form-control"
-                            id="exampleInputName"
-                            placeholder="Enter Your Name"
-                            required
-                            autoFocus
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <input
-                            type="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            className="form-control"
-                            id="exampleInputEmail"
-                            placeholder="Enter Your Email "
-                            required
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <input
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            className="form-control"
-                            id="exampleInputPassword"
-                            placeholder="Enter Your Password"
-                            required
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <input
-                            type="text"
-                            value={phone}
-                            onChange={(e) => setPhone(e.target.value)}
-                            className="form-control"
-                            id="exampleInputPhone"
-                            placeholder="Enter Your Phone"
-                            required
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <input
-                            type="text"
-                            value={address}
-                            onChange={(e) => setAddress(e.target.value)}
-                            className="form-control"
-                            id="exampleInputAddress"
-                            placeholder="Enter Your Address"
-                            required
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <input
-                            type="text"
-                            value={answer}
-                            onChange={(e) => setAnswer(e.target.value)}
-                            className="form-control"
-                            id="exampleInputAnswer"
-                            placeholder="What is Your Favorite sports"
-                            required
-                        />
-                    </div>
-                    <button type="submit" className="btn btn-primary">
-                        REGISTER
-                    </button>
-                </form>
-            </div>
-        </Layout>
+        <>
+
+            <Layout title="Register - Gerald e-shop">
+                <div className="form-container" style={{ minHeight: "93vh" }}>
+                    <form onSubmit={handleSubmit}>
+                        <h4 className="title">REGISTER FORM</h4>
+                        <div className="mb-3">
+                            <input
+                                type="text"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                                className="form-control"
+                                id="exampleInputName"
+                                placeholder="Enter Your Name"
+                                required
+                                autoFocus
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <input
+                                type="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                className="form-control"
+                                id="exampleInputEmail"
+                                placeholder="Enter Your Email "
+                                required
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <input
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className="form-control"
+                                id="exampleInputPassword"
+                                placeholder="Enter Your Password"
+                                required
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <input
+                                type="text"
+                                value={phone}
+                                onChange={(e) => setPhone(e.target.value)}
+                                className="form-control"
+                                id="exampleInputPhone"
+                                placeholder="Enter Your Phone"
+                                required
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <input
+                                type="text"
+                                value={address}
+                                onChange={(e) => setAddress(e.target.value)}
+                                className="form-control"
+                                id="exampleInputAddress"
+                                placeholder="Enter Your Address"
+                                required
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <input
+                                type="text"
+                                value={answer}
+                                onChange={(e) => setAnswer(e.target.value)}
+                                className="form-control"
+                                id="exampleInputAnswer"
+                                placeholder="What is Your Favorite sports"
+                                required
+                            />
+                        </div>
+                        <button type="submit" className="btn btn-primary">
+                            REGISTER
+                        </button>
+                    </form>
+                </div>
+            </Layout>
+        </>
     );
 };
 
