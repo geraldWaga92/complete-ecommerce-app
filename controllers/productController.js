d90584f63cdbfce5e3060cc907cd6f6756cbd9a7
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-05-02 10:11:55 -0700

    install express formidable and create product controller

---
+++ controllers/productController.js
@@ -0,0 +1,56 @@
+import productModel from "../models/productModel.js";
+// import categoryModel from "../models/categoryModel.js";
+// import orderModel from "../models/orderModel.js";
+
+import fs from "fs";
+import slugify from "slugify";
+// import braintree from "braintree";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+export const createProductController = async (req, res) => {
+    try {
+        const { name, description, price, category, quantity } =
+            req.fields;
+        const { photo } = req.files;
+
+        //validation
+        switch (true) {
+            case !name:
+                return res.status(500).send({ error: "Name is Required" });
+            case !description:
+                return res.status(500).send({ error: "Description is Required" });
+            case !price:
+                return res.status(500).send({ error: "Price is Required" });
+            case !category:
+                return res.status(500).send({ error: "Category is Required" });
+            case !quantity:
+                return res.status(500).send({ error: "Quantity is Required" });
+            case photo && photo.size > 1000000:
+                return res
+                    .status(500)
+                    .send({ error: "photo is Required and should be less then 1mb" });
+        }
+
+        const products = new productModel({ ...req.fields, slug: slugify(name) });
+        if (photo) {
+            products.photo.data = fs.readFileSync(photo.path);
+            products.photo.contentType = photo.type;
+        }
+        await products.save();
+        res.status(201).send({
+            success: true,
+            message: "Product Created Successfully",
+            products,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            error,
+            message: "Error in crearing product",
+        });
+    }
+};
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import morgan from "morgan";
 import cors from "cors";
 import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
-// import productRoutes from "./routes/productRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 
 
 //configure env
@@ -27,7 +27,7 @@ app.use(morgan("dev"));
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
-// app.use("/api/v1/product", productRoutes);
+app.use("/api/v1/product", productRoutes);
 
 
 
