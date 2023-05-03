73166358d152db7d0bf210596a4e900652af264a
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-05-03 13:25:38 -0700

    update header with categories option routes

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import Logo from "../../logofinal4.png";
 import { useAuth } from "../../context/auth";
 import toast from "react-hot-toast";
 import SearchInput from "../Form/SearchInput";
+import useCategory from "../../hooks/useCategory";
 
 const Header = () => {
     const [auth, setAuth] = useAuth();
+    const categories = useCategory();
 
     const handleLogout = () => {
         setAuth({
@@ -54,11 +56,34 @@ const Header = () => {
                                     Home
                                 </NavLink>
                             </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/category">
-                                    Category
-                                </NavLink>
+                            <li className="nav-item dropdown">
+                                <Link
+                                    className="nav-link dropdown-toggle"
+                                    to={"/categories"}
+                                    data-bs-toggle="dropdown"
+                                >
+                                    Categories
+                                </Link>
+                                <ul className="dropdown-menu">
+                                    <li>
+                                        <Link className="dropdown-item" to={"/categories"}>
+                                            All Categories
+                                        </Link>
+                                    </li>
+                                    {categories?.map((c, i) => (
+                                        <li key={i}>
+                                            <Link
+                                                className="dropdown-item"
+                                                to={`/category/${c.slug}`}
+                                            >
+                                                {c.name}
+                                            </Link>
+                                        </li>
+                                    ))}
+                                </ul>
                             </li>
+
+
                             {!auth.user ? (
                                 <>
                                     <li className="nav-item">
